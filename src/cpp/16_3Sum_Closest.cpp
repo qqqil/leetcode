eab6f1c600ccc088fdc67f8bfb4898995633cce4
andy <qqqil@126.com>
2017-02-22 00:32:28 +0800

    3Sum Closest

---
+++ src/cpp/16_3Sum_Closest.cpp
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+//
+// Created by andy on 17-2-22.
+//
+int threeSumClosest(vector<int>& nums, int target) {
+
+    sort(nums.begin(),nums.end());
+    size_t  len = nums.size();
+    int min = nums[0]+nums[1]+nums[2];
+    for(int i=0;i<len-2;i++){
+        int start = i+1;
+        int end = len -1;
+        int delt = nums[i]+nums[start]+nums[end];
+        bool is_start = true;
+        while(start != end){
+
+            int sum = nums[i]+nums[start]+nums[end];
+            if(is_start){
+                delt = sum;
+                is_start = false;
+            }else{
+                if(abs(target - sum) <=abs(target - delt)){
+                    delt = sum;
+                }
+                if(delt == target){
+                    break;
+                }else if(sum >target){
+                    end--;
+                }else{
+                    start++;
+                }
+            }
+        }
+        if(abs(target - delt) < abs(target - min)){
+            min = delt;
+        }
+    }
+    return min;
+}
