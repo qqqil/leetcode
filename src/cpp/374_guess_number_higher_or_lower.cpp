843cc9f615b1f090d20d87bc3c3dc3621a2318e8
andy <qqqil@126.com>
2017-03-15 23:54:09 +0800

    167 two sum2

---
+++ src/cpp/374_guess_number_higher_or_lower.cpp
@@ -0,0 +1,31 @@
+//
+// Created by andy on 17-3-15.
+//374_guess_number_higher_or_lower
+//
+
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int lo =1;
+        int hi = n;
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            int rlt = guess(mid);
+            if(rlt == 0){
+                return mid;
+            }
+            if(rlt == 1){
+                lo = mid +1;
+            }else{
+                hi = mid -1;
+            }
+
+        }
+        return n;
+    }
+};
