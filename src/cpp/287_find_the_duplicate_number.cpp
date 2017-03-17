17bceec881a4b86ab90ce9fb811ba41bbe4843e6
andy <qqqil@126.com>
2017-03-17 23:21:56 +0800

    find the duplicate number

---
+++ src/cpp/287_find_the_duplicate_number.cpp
@@ -0,0 +1,29 @@
+//
+// Created by andy on 17-3-17.
+//
+
+//complexity:time O(N),space O(1)
+class Solution {
+public:
+int findDuplicate(vector<int>& nums) {
+    if (nums.size() > 1)
+    {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        fast = 0;
+        while (fast != slow)
+        {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return slow;
+    }
+    return -1;
+}
+};
